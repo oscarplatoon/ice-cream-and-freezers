9392552c15c060da9936ce7b62c65c61fac48942
Won Yang <won@gatech.edu>
2021-06-04 16:16:12 -0500

    adds solution, idk what law of demeter is lol

--- ice_cream_and_freezers.py
+++ ice_cream_and_freezers.py
@@ -1,8 +1,50 @@
 class IceCream:
+    def __init__(self, name, freezing_time):
+        self.name = name
+        self.freezing_time = freezing_time
+        self.status = ""
+        self.update_status()
+
+    def update_status(self):
+        if self.freezing_time >= 2:
+            self.status = "watery"
+        elif self.freezing_time == 1:
+            self.status = "almost_ready"
+        elif self.freezing_time == 0:
+            self.status = "ready"
+        else:
+            self.status = "freezer_burned"
 
 
 class Freezer:
+    def __init__(self):
+        self.ice_creams = []  # will contain IceCream instances
+
+    def put(self, ice_cream):
+        self.ice_creams.append(ice_cream)
+
+    def remove(self, ice_cream_name):
+        for ice_cream in self.ice_creams:
+            if ice_cream.name == ice_cream_name:
+                self.ice_creams.remove(ice_cream)
+
+    def return_ice_cream(self):
+        arr_to_return = []
+        for ice_cream in self.ice_creams:
+            ice_cream.update_status()
+            arr_to_return.append((ice_cream.name, ice_cream.status))
+        return arr_to_return
+
+    def freeze(self):
+        for ice_cream in self.ice_creams:
+            ice_cream.freezing_time -= 1
+            ice_cream.update_status()
 
 
-# Will you need other classes? What attributes will those classes have?
-# How will classes talk to each other?
+fr = Freezer()
+choco = IceCream("Chocolate", 3)
+vanil = IceCream("Vanilla", 2)
+
+fr.put(choco)
+fr.put(vanil)
+print(fr.return_ice_cream())
--- ice_cream_and_freezers_spec.py
+++ ice_cream_and_freezers_spec.py
@@ -3,7 +3,41 @@ from ice_cream_and_freezers import IceCream, Freezer
 
 
 class TestIceCreamAndFreezers(unittest.TestCase):
-    # Go ahead and write some tests here for your code. We know that you'll at least need the IceCream and Freezer class.
+    def setUp(self):
+        self.frzr = Freezer()
+        self.choco = IceCream("Chocolate", 3)
+        self.vanil = IceCream("Vanilla", 2)
+
+    def test_put_ice_cream_into_freezer(self):
+        self.frzr.put(self.choco)
+        self.frzr.put(self.vanil)
+        self.assertEqual(self.frzr.return_ice_cream(), [
+                         ("Chocolate", "watery"), ("Vanilla", "watery")])
+
+    def test_take_ice_cream_out(self):
+        self.frzr.put(self.choco)
+        self.frzr.put(self.vanil)
+        self.frzr.remove("Chocolate")
+        self.assertEqual(self.frzr.return_ice_cream(), [("Vanilla", "watery")])
+
+    def test_freeze_ice_cream(self):
+        self.frzr.put(IceCream("Chocolate", 3))
+        self.frzr.put(IceCream("Vanilla", 2))
+        self.frzr.freeze()
+        self.frzr.freeze()
+        self.assertEqual(self.frzr.return_ice_cream(), [
+                         ("Chocolate", "almost_ready"), ("Vanilla", "ready")])
+
+    def test_burn_freezer(self):
+        self.frzr.put(IceCream("Chocolate", 3))
+        self.frzr.put(IceCream("Vanilla", 2))
+        self.frzr.freeze()
+        self.frzr.freeze()
+        self.frzr.freeze()
+        self.frzr.freeze()
+        self.assertEqual(self.frzr.return_ice_cream(), [
+                         ("Chocolate", "freezer_burned"), ("Vanilla", "freezer_burned")])
+
 
 if __name__ == '__main__':
     unittest.main()
