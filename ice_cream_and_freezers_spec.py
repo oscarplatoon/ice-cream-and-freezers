0222b3b4bf28bed9fa380f67232f6a3084a48c8e
Jeremiah Mauga <miahmauga@gmail.com>
2021-06-10 15:03:56 -0700

    Jeremiah and Don's progress for Icecream

--- ice_cream_and_freezers_spec.py
+++ ice_cream_and_freezers_spec.py
@@ -2,8 +2,8 @@ import unittest
 from ice_cream_and_freezers import IceCream, Freezer
 
 
-class TestIceCreamAndFreezers(unittest.TestCase):
-    # Go ahead and write some tests here for your code. We know that you'll at least need the IceCream and Freezer class.
+# class TestIceCreamAndFreezers(unittest.TestCase):
+#     # Go ahead and write some tests here for your code. We know that you'll at least need the IceCream and Freezer class.
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
