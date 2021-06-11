75efbd7138f7b35235025c9811fcbeb88549da1f
Marc Stanley <marcson909@gmail.com>
2021-06-10 23:17:55 -0500

    updated with unit tests

--- freezer.py
+++ freezer.py
@@ -1,16 +1,13 @@
-from ice_cream import IceCream
-
 class Freezer:
-    max_storage = 10
 
 
-    def __init__(self, size = max_storage):
+    def __init__(self):
        self.storage = []
-       self.size = size
 
     def __str__(self) -> str:
-        temp = 'FREEZER CONTENTS: \n-----------\n'
-
+        temp = 'FREEZER CONTENTS: \n-----------------\n'
+        if self.storage == []:
+            temp += "Empty"
         for s in self.storage:
             temp += str(s) + "\n"
         return temp
@@ -18,12 +15,21 @@ class Freezer:
     def insert_ice_cream(self, new_ice_cream):
         self.storage.append(new_ice_cream)
 
-    def get_ice_cream(self, flavor):
+    def get_ice_cream(self):
+        ice_list = []
         for ice_cream in self.storage:
-            if ice_cream.flavor == flavor:
-                self.storage.remove(ice_cream)
-                return ice_cream
+            ice_list.append((ice_cream.flavor,ice_cream.status))
+        return ice_list
 
     def remove_ice(self, ice_cream):
         self.storage.remove(ice_cream)
         return ice_cream.flavor
+    
+    def list_freeze_contents(self, num):
+        
+        for ice in self.storage:
+            ice.set_time_in_freeze(num)
+            ice.update_status()
+            if ice.status == 'ready':
+                print(f"The {ice.flavor} ice cream is ready to be taken out of the freezer")
+                print(f"I'm going to eat the {self.remove_ice(ice)} ice cream")
--- runner.py
+++ runner.py
@@ -9,13 +9,16 @@ delish_ice = IceCream('Delish', 'h')
 freezer.insert_ice_cream(chocolate_icecream)
 freezer.insert_ice_cream(pb_ice)
 freezer.insert_ice_cream(delish_ice)
+
 print(freezer)
 
-for ice in freezer.storage:
-    ice.set_time_in_freeze(6)
-    ice.update_status()
-    if ice.status == 'ready':
-        print("I'm going to eat", freezer.remove_ice(ice), "ice cream")
+freezer.list_freeze_contents(6)
+
+# for ice in freezer.storage:
+#     ice.set_time_in_freeze(6)
+#     ice.update_status()
+#     if ice.status == 'ready':
+#         print("I'm going to eat", freezer.remove_ice(ice), "ice cream")
 
 # print(freezer)
 
